6d9d6a2326bf3d5014c98231507f61599bbbecfa
Roginth B <rohinthrewa248@gmail.com>
2025-11-14 00:18:41 +0530

    ui and bug fix

--- src/web_ui.py
+++ src/web_ui.py
@@ -18,7 +18,7 @@ class Ui:
 
     def home_page(self):
 
-        st.title("AskQ")
+        st.title("Ask:yellow[Q] :bot")
         if "messages" not in st.session_state:
             st.session_state.messages =[]
         
@@ -42,13 +42,13 @@ class Chat_with_OpenAI:
         pass
     def start(self):
         load_dotenv()
-        st.title("askQ")
+        st.title("Ask:blue[Q]")
 
         client =OpenAI(
             base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
         )
         if "openai_model" not in st.session_state:
-            st.session_state["openai_model"]="gemini-2.5-flash"
+            st.session_state["openai_model"]="models/gemini-2.5-flash"
         if "messages" not in st.session_state:
             st.session_state.messages =[]
 
@@ -56,19 +56,57 @@ class Chat_with_OpenAI:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
 
-        if prompt :=st.chat_input("what is up?"):
+        if prompt :=st.chat_input("Ask something..."):
             st.session_state.messages.append({"role":"user", "content": prompt})
             with st.chat_message("user"):
                 st.markdown(prompt)
+        if len(st.session_state.get("messages", [])) == 0:
+                #st.("No messages to send!")
+                #st.info("Hello!, this is AskQ how can I assist today?")
+                st.html("""
+                        <div style="
+                            text-align: center;
+                            
+                            padding: 30px;
+                            border-radius: 12px;
+                            box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+                        ">
+                            <h3 style="
+                                color: #2b6cb0;
+                                font-family: 'Segoe UI', sans-serif;
+                                font-size: 28px;
+                                margin-bottom: 10px;
+                            ">
+                                Welcome to AskQ AI
+                            </h3>
 
-        with st.chat_message("assistant"):
-            stream =client.chat.completions.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content":m["content"]} 
-                    for m in st.session_state.messages
-                ],
-                stream=True,
-            )
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role":"assistant", "content":response})
+                            <p style="
+                                color: #4a5568;
+                                font-family: 'Segoe UI', sans-serif;
+                                font-size: 16px;
+                                margin-top: 0;
+                            ">
+                                Ask anything, and I'll help you find the answer instantly.
+                            </p>
+                        </div>
+                        """)
+        else:
+            with st.chat_message("assistant"):
+            # stream =client.chat.completions.create(
+            #     model=st.session_state["openai_model"],
+            #     messages=[
+            #         {"role": m["role"], "content":m["content"]} 
+            #         for m in st.session_state.messages
+            #     ],
+            #     stream=True,
+            # )
+
+
+                with st.spinner("Thinking...."):
+                    stream = client.chat.completions.create(
+                        model=st.session_state["openai_model"],
+                        messages=st.session_state["messages"],
+                        stream=True
+                    )
+                    response = st.write_stream(stream)
+                    st.session_state.messages.append({"role":"assistant", "content":response})
